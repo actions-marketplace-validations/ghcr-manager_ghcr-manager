cf8123711d63d3185aa0cd8b0d88309b3cc84d71
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2026-04-29 18:11:05 +0200

    Make action scan-only and remove command input

--- src/action/_main.ts
+++
@@ -1,7 +0,0 @@
-import { runAction } from "./index.js";
-
-runAction().catch((error: unknown) => {
-  const message = error instanceof Error ? error.message : String(error);
-  console.error(message);
-  process.exitCode = 1;
-});
